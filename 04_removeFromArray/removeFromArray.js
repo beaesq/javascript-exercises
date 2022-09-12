cf78da4c0b5d5260dedd52f62ded9b1616c40ffb
Bwa <40144023+beaesq@users.noreply.github.com>
2022-09-12 23:41:01 +0800

    04-07

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
 
+  let numberOfArgumentsToRemove = arguments.length - 1;
+
+  for (let i = 1; i <= numberOfArgumentsToRemove; i++) {
+    let indexToRemove = array.findIndex((arg) => (arg === arguments[i])) ;
+    //console.log(indexToRemove);
+    if (indexToRemove >= 0) {
+      array.splice(indexToRemove, 1);
+    }
+  }
+  
+  return array;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
